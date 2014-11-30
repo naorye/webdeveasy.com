e508df390778be1d6ce4c4f7ceb71db149fa8f77
Naor Ye <naorye@gmail.com>
2014-12-01 01:40:44 +0200

    - no follow

--- content/themes/casper-child/header.php
+++ content/themes/casper-child/header.php
@@ -10,6 +10,8 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="robots" content="noindex,nofollow"/>
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="HandheldFriendly" content="True" />
