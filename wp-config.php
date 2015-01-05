ce8f2c80378254c7058832877c416ea625398ed6
Naor Ye <naorye@gmail.com>
2015-01-06 01:12:34 +0200

    - Move DOMAIN_CURRENT_SITE config to local-config.php

--- wp-config.php
+++ wp-config.php
@@ -71,7 +71,6 @@ define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack t
 // =========
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'www.webdeveasy.com');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
